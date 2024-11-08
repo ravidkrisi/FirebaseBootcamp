b7d54998a9c0036a963ec5aa637d548aa780aa7b
ravidkrisi <ravidkrisi22@gmail.com>
2024-11-08 16:07:20 +0700

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Ravid Krisi on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
